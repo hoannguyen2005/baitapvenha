513bf4be2b473399a43912d999233224eb814789
hoannguyen2005 <hoannguyenfuter@gmail.com>
2024-09-18 21:39:13 +0700

    bài tập

---
+++ bai1.js
@@ -0,0 +1,6 @@
+
+    let a=+prompt("Nhập điểm môn toán" );
+    let b=+prompt("Nhập điểm môn lý" );
+    let c=+prompt("Nhập điểm môn hóa" );
+    let diemtrungbinh = (a+b+c)/3;
+    alert(diemtrungbinh);
